d6d47fc73474fb93e9b13af171cf5defb3e767eb
EC2 Default User <ec2-user@ip-172-31-65-234.ec2.internal>
2022-01-06 11:49:58 +0000

    [Add]admin_items_views作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
 end
